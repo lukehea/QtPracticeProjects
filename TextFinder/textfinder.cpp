af1a97cde75a981f5aa2a4d313288d6748f5be82
Luke Headington <lualhe416@gmail.com>
2023-11-13 22:26:47 -0500

    finished textfinder practice project

--- TextFinder/textfinder.cpp
+++ TextFinder/textfinder.cpp
@@ -1,11 +1,11 @@
 #include "textfinder.h"
-#include "./ui_textfinder.h"
 
 TextFinder::TextFinder(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::TextFinder)
 {
     ui->setupUi(this);
+    loadTextFile();
 }
 
 TextFinder::~TextFinder()
@@ -13,3 +13,23 @@ TextFinder::~TextFinder()
     delete ui;
 }
 
+
+void TextFinder::on_findButton_clicked()
+{
+    QString stringToSearch = ui->lineEdit->text();
+    ui->textEdit->find(stringToSearch, QTextDocument::FindWholeWords);
+}
+
+void TextFinder::loadTextFile()
+{
+    QFile inputFile(":/input.txt");
+    inputFile.open(QIODevice::ReadOnly);
+
+    QTextStream inStream(&inputFile);
+    QString fileLine = inStream.readAll();
+    inputFile.close();
+
+    ui->textEdit->setPlainText(fileLine);
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
+}
--- TextFinder/textfinder.h
+++ TextFinder/textfinder.h
@@ -2,6 +2,12 @@
 #define TEXTFINDER_H
 
 #include <QMainWindow>
+#include <QIODevice>
+#include <QFile>
+#include <QTextStream>
+
+#include "./ui_textfinder.h"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class TextFinder; }
@@ -15,7 +21,11 @@ public:
     TextFinder(QWidget *parent = nullptr);
     ~TextFinder();
 
+private slots:
+    void on_findButton_clicked();
+
 private:
     Ui::TextFinder *ui;
+    void loadTextFile();
 };
 #endif // TEXTFINDER_H
